a334860fb383f7b6b8c5220f6acb0e272ea7982a
steak.guo <steak.guo@lunarax.io>
2023-06-21 09:48:31 +0800

    对于除0的默认处理

--- internal/core/math.go
+++ internal/core/math.go
@@ -160,63 +160,93 @@ func Div(a, b reflect.Value) (interface{}, error) {
 	akind := a.Kind().String()
 	bkind := b.Kind().String()
 
-	if strings.HasPrefix(bkind, "int") {
-		bi := b.Int()
-		if bi == 0 {
-			return nil, errors.New("DIV(/) can't be used to Div ZERO(0)!")
-		}
-	}
-	if strings.HasPrefix(bkind, "uint") {
-		bu := b.Uint()
-		if bu == 0 {
-			return nil, errors.New("DIV(/) can't be used to Div ZERO(0)!")
-		}
-	}
-	if strings.HasPrefix(bkind, "float") {
-		bf := b.Float()
-		if bf == 0.0 {
-			return nil, errors.New("DIV(/) can't be used to Div ZERO(0)!")
-		}
-	}
+	//if strings.HasPrefix(bkind, "int") {
+	//	bi := b.Int()
+	//	if bi == 0 {
+	//		return nil, errors.New("DIV(/) can't be used to Div ZERO(0)!")
+	//	}
+	//}
+	//if strings.HasPrefix(bkind, "uint") {
+	//	bu := b.Uint()
+	//	if bu == 0 {
+	//		return nil, errors.New("DIV(/) can't be used to Div ZERO(0)!")
+	//	}
+	//}
+	//if strings.HasPrefix(bkind, "float") {
+	//	bf := b.Float()
+	//	if bf == 0.0 {
+	//		return nil, errors.New("DIV(/) can't be used to Div ZERO(0)!")
+	//	}
+	//}
 
+	// customize div zero logic
+	// be caution when use
 	if strings.HasPrefix(akind, "int") {
 		if strings.HasPrefix(bkind, "int") {
+			// return a*10 when b is zero, not an error
+			if b.Int() == 0 {
+				return a.Int() * 10, nil
+			}
 			return a.Int() / b.Int(), nil
 		}
 
 		if strings.HasPrefix(bkind, "uint") {
+			if b.Uint() == 0 {
+				return a.Int() * 10, nil
+			}
 			return a.Int() / int64(b.Uint()), nil
 		}
 
 		if strings.HasPrefix(bkind, "float") {
+			if b.Float() == 0.0 {
+				return a.Int() * 10, nil
+			}
 			return float64(a.Int()) / b.Float(), nil
 		}
 	}
 
 	if strings.HasPrefix(akind, "uint") {
 		if strings.HasPrefix(bkind, "int") {
+			if b.Int() == 0 {
+				return a.Uint() * 10, nil
+			}
 			return int64(a.Uint()) / b.Int(), nil
 		}
 
 		if strings.HasPrefix(bkind, "uint") {
+			if b.Uint() == 0 {
+				return a.Uint() * 10, nil
+			}
 			return a.Uint() / b.Uint(), nil
 		}
 
 		if strings.HasPrefix(bkind, "float") {
+			if b.Float() == 0.0 {
+				return a.Uint() * 10, nil
+			}
 			return float64(a.Uint()) / b.Float(), nil
 		}
 	}
 
 	if strings.HasPrefix(akind, "float") {
 		if strings.HasPrefix(bkind, "int") {
+			if b.Int() == 0 {
+				return a.Float() * 10.0, nil
+			}
 			return a.Float() / float64(b.Int()), nil
 		}
 
 		if strings.HasPrefix(bkind, "uint") {
+			if b.Uint() == 0 {
+				return a.Float() * 10.0, nil
+			}
 			return a.Float() / float64(b.Uint()), nil
 		}
 
 		if strings.HasPrefix(bkind, "float") {
+			if b.Float() == 0.0 {
+				return a.Float() * 10.0, nil
+			}
 			return a.Float() / b.Float(), nil
 		}
 	}
--- test/math/num_test.go
+++ test/math/num_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/cookedsteak/gengine/builder"
 	"github.com/cookedsteak/gengine/context"
 	"github.com/cookedsteak/gengine/engine"
@@ -61,4 +63,41 @@ func exec_num() {
 
 func Test_num(t *testing.T) {
 	exec_num()
+}
+
+func TestDivZero(t *testing.T) {
+	zero_rule := `
+	rule "div zero" "rule desc"
+	begin
+	entity.Height = 10/entity.Score
+	end`
+
+	entity := &Entity{Score: 0}
+	dataContext := context.NewDataContext()
+	dataContext.Add("entity", entity)
+
+	//init rule engine
+	ruleBuilder := builder.NewRuleBuilder(dataContext)
+
+	//读取规则
+	start1 := time.Now().UnixNano()
+	err := ruleBuilder.BuildRuleFromString(zero_rule)
+	end1 := time.Now().UnixNano()
+
+	println(fmt.Sprintf("rules num:%d, load rules cost time:%d ns", len(ruleBuilder.Kc.RuleEntities), end1-start1))
+
+	if err != nil {
+		panic(err)
+	}
+	eng := engine.NewGengine()
+
+	start := time.Now().UnixNano()
+	// true: means when there are many rules， if one rule execute error，continue to execute rules after the occur error rule
+	err = eng.Execute(ruleBuilder, true)
+	end := time.Now().UnixNano()
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, 100.0, entity.Height, "return 100 not error")
+	println(fmt.Sprintf("execute rule cost %d ns", end-start))
 }
